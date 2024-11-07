377cc58f3c762238dbb276b6c11ddf50b860d202
matrix-09 <129373323+matrix-09@users.noreply.github.com>
2024-11-07 20:19:00 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -30,7 +30,7 @@ app.get('*', (req, res) => {
   });
 
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+    //console.log(`Server running on port ${PORT}`);
 });
 
 cron.schedule('*/5 * * * *', () => {
